b518f1f194d5eec936b91eef88d0f966c3326a0d
jobtalle <jobtalle@hotmail.com>
2014-12-03 22:47:28 +0100

    Added header

---
+++ include/ccSort/ccSort.h
@@ -0,0 +1,28 @@
+//__________________________________________________________________________________//
+//                                  _____            _                              //
+//                                 / ____|          | |                             //
+//                         ___ ___| (___   ___  _ __| |_                            //
+//                        / __/ __|\___ \ / _ \| '__| __|                           //
+//                       | (_| (__ ____) | (_) | |  | |_                            //
+//                        \___\___|_____/ \___/|_|   \__| 1.0                       //
+//                                                                                  //
+//             Copyright (C) 2014 \ Job Talle (job@ccore.org)                       //
+//__________________________________________________________________________________//
+//                                                                                  //
+//      This program is free software: you can redistribute it and/or modify        //
+//      it under the terms of the 3-clause BSD license.                             //
+//                                                                                  //
+//      You should have received a copy of the 3-clause BSD License along with      //
+//      this program. If not, see <http://opensource.org/licenses/>.                //
+//__________________________________________________________________________________//
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
